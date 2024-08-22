6905e0801dbbdc7ebfa2846eefe61d5ebea51c09
Kirit Suthar <Kiritinfolabz@gmail.com>
2024-08-22 15:03:28 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import pandas as pd
-st.set_page_config(page_icon=":exclamation:",page_title="About AI-Driven Visual Insights for Wellness and Sustainable Harvests")
+from urllib.parse import urlencode
+
+st.set_page_config(page_icon=":exclamation:", page_title="About AI-Driven Visual Insights for Wellness and Sustainable Harvests")
 
 st.title("Welcome:- About AI-Driven Visual Insights for Wellness and Sustainable Harvests")
 st.title("LOGIN ")
 
-# To hide side bar we use :-
-
+# To hide side bar
 st.markdown(
     """
-        <style>
+    <style>
     [data-testid="stSidebar"]{   
         visibility: hidden;
     }
@@ -26,20 +27,26 @@ data = pd.DataFrame(df)
 
 # Get the user input
 email = st.text_input("Enter email")
-password = st.text_input("Enter Password",type="password")
+password = st.text_input("Enter Password", type="password")
 
 # Check if the user clicked the login button
 try:
     if st.button("Login"):
         if data[data["Email"] == email]["Password"].values[0] == password:
             st.success("Login successful")
-            st.markdown(f'<meta http-equiv="refresh" content="2;url=http://localhost:8501/main">', unsafe_allow_html=True)
-            st.header("Redirecting...")
+            # Redirect by appending parameters to the URL
+            params = urlencode({"page": "main"})
+            st.experimental_set_query_params(**{"page": "main"})
+            st.experimental_rerun()
         else:
             st.error("Invalid Email Or Password")
 except:
     st.warning("Enter email And Password")
 
+link = '[Register](http://localhost:8501/Signup)'
+st.markdown(link, unsafe_allow_html=True)
 
-link='[Register](http://localhost:8501/Signup)'
-st.markdown(link,unsafe_allow_html=True)
+# Handling page redirection
+query_params = st.experimental_get_query_params()
+if query_params.get("page") == ["main"]:
+    st.write("Welcome to the main page!")
